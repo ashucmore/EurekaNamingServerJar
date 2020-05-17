395ac756d466bfbcbbccf93b191e5d0ba164c01c
Ashutosh C More <ashucmore@gmail.com>
2020-05-17 17:10:45 +0530

    Spring initializer is done

--- src/main/java/com/example/demo/EurakaApplication.java
+++ src/main/java/com/example/demo/EurakaApplication.java
@@ -3,12 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurakaApplication {
+public class EurakaApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurakaApplication.class, args);
